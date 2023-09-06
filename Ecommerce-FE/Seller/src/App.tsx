b0ef2566e4351a740514147563cb2eb344ac200a
ChauVanLoc <19130121@st.hcmuaf.edu.vn>
2023-09-06 19:19:17 +0700

    header of seller

--- Ecommerce-FE/Seller/src/App.tsx
+++ Ecommerce-FE/Seller/src/App.tsx
@@ -1,5 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import React from 'react'
+import Home from './Pages/Home'
+import Header from './Components/Header'
+
+const Login = React.lazy(() => import('./Pages/Login'))
+const Register = React.lazy(() => import('./Pages/Register'))
+
 function App() {
-  return <div></div>;
+  return (
+    <div className='base'>
+      <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
