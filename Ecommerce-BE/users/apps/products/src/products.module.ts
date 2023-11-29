e766d6d8991d135e64d9f8384f7a495af86be125
ERWIN\erwin <19130120@st.hcmuaf.edu.vn>
2023-11-29 22:20:56 +0700

    update product api

--- Ecommerce-BE/users/apps/products/src/products.module.ts
+++ Ecommerce-BE/users/apps/products/src/products.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { ProductsController } from './products.controller'
 import { ProductsService } from './products.service'
 import { ConfigModule } from '@app/common'
+import { ProductController } from './product.controller';
 
 @Module({
   imports: [ConfigModule],
-  controllers: [ProductsController],
+  controllers: [ProductsController, ProductController],
   providers: [ProductsService]
 })
 export class ProductsModule {}
