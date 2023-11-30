fca24367fe259f3c0e67eb66ff71d9edee6e6906
ERWIN\erwin <19130120@st.hcmuaf.edu.vn>
2023-11-30 21:24:51 +0700

    update schema

--- Ecommerce-BE/users/apps/products/src/products.module.ts
+++ Ecommerce-BE/users/apps/products/src/products.module.ts
@@ -2,12 +2,7 @@ import { Module } from '@nestjs/common'
 import { ProductsController } from './products.controller'
 import { ProductsService } from './products.service'
 import { ConfigModule } from '@app/common'
-<<<<<<< HEAD
-import { ProductController } from './product.controller';
 
-@Module({
-  imports: [ConfigModule],
-=======
 import { ClientsModule, Transport } from '@nestjs/microservices'
 import { ConfigService } from '@nestjs/config'
 import { ProductController } from './product.controller'
@@ -36,7 +31,7 @@ import { ProductController } from './product.controller'
     }),
     ConfigModule
   ],
->>>>>>> 466cc0ffa56ac06bf5ae746f9af42236e4042fd0
+
   controllers: [ProductsController, ProductController],
   providers: [ProductsService]
 })
