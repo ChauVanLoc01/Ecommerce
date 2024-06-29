91ee09e94c9554e8864e14db98d8d7d4f07cb2d3
ChauVanLoc <chauvanloc.tg@gmail.com>
2024-06-29 21:11:14 +0700

    update

--- Ecommerce-FE/admin/src/pages/Rating/RatingForm.tsx
+++ Ecommerce-FE/admin/src/pages/Rating/RatingForm.tsx
@@ -1,4 +1,4 @@
-import { AlertDialog, Box, Button, Flex, Spinner, Text, TextArea } from '@radix-ui/themes'
+import { AlertDialog, Avatar, Box, Button, Flex, Spinner, Text, TextArea } from '@radix-ui/themes'
 import { format } from 'date-fns'
 import MultiUploadFile from 'src/components/MultiUploadFile/MultiUploadFile'
 import RatingReadOnly from 'src/components/Rating/RatingReadOnly'
@@ -40,42 +40,48 @@ function ReplyRatingCreate({
 }: ReplyRatingCreateProps) {
     return (
         <AlertDialog.Root open={openReplyRating} onOpenChange={setOpenReplyRating}>
-            <AlertDialog.Content maxWidth='900px' className='!rounded-8'>
+            <AlertDialog.Content maxWidth='700px' className='!rounded-8'>
                 <form>
                     <div className='space-y-5'>
                         <AlertDialog.Title>Phản hồi đánh giá</AlertDialog.Title>
-
                         <Flex direction='column' gap='3'>
-                            <div
-                                id={user.full_name}
-                                className='user-rating flex flex-row w-full h-full items-center gap-3'
-                            >
-                                <p className='text-base font-bold'>{user.full_name}</p>
-                                <p className='text-sm font-bold'>{user.email}</p>
+                            <Flex gap='3' align='center'>
+                                <Avatar size='3' src={user.image} radius='full' fallback='T' />
+                                <Box>
+                                    <Text as='div' size='2' weight='bold'>
+                                        {user.full_name}
+                                    </Text>
+                                    <Text as='div' size='2' color='gray'>
+                                        {user.email}
+                                    </Text>
+                                </Box>
+                            </Flex>
+                            <Flex className='space-x-4 items-center'>
                                 <RatingReadOnly ratingValue={rating.stars} />
-                                <p>{format(rating.createdAt, formatDefault)}</p>
-                            </div>
-                            <div className='user-comment flex flex-col w-full h-full flex-start gap-5'>
-                                <div className='flex flex-col w-full h-full items-start gap-3 px-5 py-3 border-2 border-gray-300 rounded-md'>
-                                    <p className='text-md'>{rating.comment}</p>
-                                </div>
-                            </div>
+                                <Text>{format(rating.createdAt, formatDefault)}</Text>
+                            </Flex>
+                            <TextArea disabled>{rating.comment}</TextArea>
                         </Flex>
                         <Flex direction='column' gap='3'>
-                            <Text as='p'>Phản hồi khách hàng (có thể để trống)</Text>
+                            <Text as='p'>Phản hồi khách hàng (có thể không cần phản hồi)</Text>
                             <Box maxWidth='100%'>
-                                <TextArea onChange={handleComment} size='3' placeholder='Nhận xét' />
+                                {
+                                    [
+                                        <TextArea
+                                            onChange={handleComment}
+                                            size='3'
+                                            rows={4}
+                                            placeholder='Thêm phản hồi'
+                                        />,
+                                        <TextArea
+                                            disabled
+                                            size='3'
+                                            placeholder='Thêm phản hồi'
+                                            defaultValue={rating.comment}
+                                        />
+                                    ][+rating.isReply]
+                                }
                             </Box>
-                            <MultiUploadFile files={files} setFiles={setFiles} min={0} cols={5}>
-                                {(total, current, min) => (
-                                    <Flex className='space-x-1 mt-2'>
-                                        <Text>Đã tải lên</Text>
-                                        <Flex>
-                                            (<Text color={current < min ? 'red' : 'blue'}>{current}</Text>/{total})
-                                        </Flex>
-                                    </Flex>
-                                )}
-                            </MultiUploadFile>
                         </Flex>
                         <Flex gap='3' mt='4' justify='end'>
                             <AlertDialog.Cancel>
@@ -83,10 +89,12 @@ function ReplyRatingCreate({
                                     Trở về
                                 </Button>
                             </AlertDialog.Cancel>
-                            <Button type='button' onClick={onCreateReplyRating} className='bg-blue text-white'>
-                                {isCreating && <Spinner />}
-                                Lưu Phản hồi
-                            </Button>
+                            {!rating.isReply && (
+                                <Button type='button' onClick={onCreateReplyRating} className='bg-blue text-white'>
+                                    {isCreating && <Spinner />}
+                                    Lưu Phản hồi
+                                </Button>
+                            )}
                         </Flex>
                     </div>
                 </form>
